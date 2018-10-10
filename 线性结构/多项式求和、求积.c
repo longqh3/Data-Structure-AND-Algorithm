81be7e5f98b090882c4ee9702b3f9e01711d4087
longqh3 <longqh3@outlook.com>
2018-10-10 16:59:28 +0800

    2018.10.10

--- 线性结构/多项式求和、求积.c
+++ 线性结构/多项式求和、求积.c
@@ -20,10 +20,11 @@ int main()
 	Poly P1,P2,Ps,Pm;
 	P1=ReadPoly();
 	P2=ReadPoly();
-	Ps=AddPoly(P1,P2);
-	Output(Ps);
 	Pm=MultiPoly(P1,P2);
 	Output(Pm);
+	printf("\n");
+	Ps=AddPoly(P1,P2);
+	Output(Ps);
 	return 0;
 }
 
@@ -34,16 +35,16 @@ Poly ReadPoly()
 	scanf("%d",&N);
 	P=(Poly)malloc(sizeof(struct PolyNode));//构建链表头空结点 
 	P->Next=NULL;
-	Rear=P;//构建尾部节点临时存放数据？是否可以这样理解？ 
-	while(N--){
+	Rear=P;//构建尾部节点临时存放数据？是否可以这样理解？或者可以理解成初始化所必须？ 
+	for(;N;N--){
 		scanf(" %d %d",&c,&e);
-		Attach(c,e,&Rear);//将当前项插入多项式尾部 
+		Attach(c,e,&Rear);//将当前项插入多项式尾部(Rear) 
 	}
-	t=P;P=P->Next;free(t);//删除临时生成的头结点，并将第一箱作为头结点
+	t=P;P=P->Next;free(t);//删除临时生成的头结点，并将第一项作为头结点(即进行收尾，把表头空结点转移至第一个有数据的结点？) 
 	return P; 
 } 
 
-void Attach(int c,int e,Poly *pRear)
+void Attach(int c,int e,Poly *pRear)//*pRear为指针的指针 
 {
 	Poly P;
 	
@@ -57,77 +58,95 @@ void Attach(int c,int e,Poly *pRear)
 
 Poly AddPoly(Poly P1,Poly P2)
 {
-	Poly front,rear,temp;
+	Poly Ps,Rear,temp;//意义和之前的ReadPoly函数中的Poly P,Rear,t一致 
 	int sum;
-	rear=(Poly)malloc(sizeof(struct PolyNode));
-	front=rear;//由front记录结果多项式链表头结点，其实本质上front此时指代的就是rear？而在后面的循环中通过Attach函数中malloc空间给P来实现链表 
+	Ps=(Poly)malloc(sizeof(struct PolyNode));
+	Ps->Next=NULL;
+	Rear=Ps;//由front记录结果多项式链表头结点，其实本质上front此时指代的就是rear？或者说要想建立一个新的链表就必须要有Rear的存在以确定位置？而在后面的循环中通过Attach函数中malloc空间给P来实现链表 
 	while(P1 && P2){
 		switch(Compare(P1->expo,P2->expo)){
 			case 1:
-				Attach(P1->coef,P1->expo,&rear);
+				Attach(P1->coef,P1->expo,&Rear);
 				P1=P1->Next;
 				break;
 			case -1:
-				Attach(P2->coef,P2->expo,&rear);
+				Attach(P2->coef,P2->expo,&Rear);
 				P2=P2->Next;
 				break;
 			case 0:
 				sum = P1->coef+P2->coef;
-				if(sum)Attach(sum,P1->expo,&rear);
+				if(sum){
+					Attach(sum,P1->expo,&Rear);
+				}
 				P1=P1->Next;
 				P2=P2->Next;
 				break;
 		}
 	}
-	//将未处理完的另一个多项式的所有结点一次复制到结果多项式中去
-	for(;P1;P1=P1->Next)Attach(P1->coef,P1->expo,&rear);
-	for(;P2;P2=P2->Next)Attach(P2->coef,P2->expo,&rear);
-	rear->Next=NULL;
-	temp=front;
-	front=front->Next;
+	//将未处理完的另一个多项式的所有结点一次复制到结果多项式中去，其本质只是通过循环来实现数据的转移 
+	for(;P1;P1=P1->Next){
+		Attach(P1->coef,P1->expo,&Rear);
+	}
+	for(;P2;P2=P2->Next){
+		Attach(P2->coef,P2->expo,&Rear);
+	}
+	Rear->Next=NULL;
+	temp=Ps;
+	Ps=Ps->Next;
 	free(temp);
-	return front;
+	return Ps;
 }
 
-int Compare(int p1,int p2)
+int Compare(int a,int b)
 {
-	if(p1>p2) return 1;
-	if(p1=p2) return 0;
-	if(p1<p2) return -1;
+	if(a>b){
+		return 1;
+	}
+	if(a<b){
+		return -1;
+	}
+	if(a=b){
+		return 0;
+	}
 }
 
 Poly MultiPoly(Poly P1,Poly P2)
 {
-	Poly Pm,rear,t1,t2,temp,P;
-	t1=P1;t2=P2;
-	Pm = (Poly)malloc(sizeof(struct PolyNode));
+	Poly Pm,Rear,Rear2,temp,temp2,P,t1,t2;
+	t2=P2;
+	Pm=(Poly)malloc(sizeof(struct PolyNode));
 	Pm->Next=NULL;
-	rear=Pm;
-	Attach(t1->coef*t2->coef,t1->expo+t2->expo,&rear);
-	t2=t2->Next;
-	rear->Next=NULL;
-	temp=Pm; Pm=Pm->Next; free(temp);
-	while(t2){
-		P = (Poly)malloc(sizeof(struct PolyNode));
+	Rear=Pm;
+	for(;t2;t2=t2->Next){
+		Attach(P1->coef*t2->coef,P1->expo+t2->expo,&Rear);
+	}
+	P1=P1->Next;temp=Pm;Pm=Pm->Next;free(temp);//得到第一个Pm
+	while(P1){
+		P=(Poly)malloc(sizeof(struct PolyNode));
 		P->Next=NULL;
-		rear=P;
-		Attach(t1->coef*t2->coef,t1->expo+t2->expo,&rear);
-		t2=t2->Next;
-		rear->Next=NULL;
-		temp=P; P=P->Next; free(temp);
-		Pm=AddPoly(P,Pm);
-	}	
+		Rear2=P;
+		t2=P2;
+		while(t2){
+			Attach(P1->coef*t2->coef,P1->expo+t2->expo,&Rear2);
+			t2=t2->Next;
+		}
+		temp2=P;P=P->Next;free(temp2);
+		Pm=AddPoly(Pm,P);
+		P1=P1->Next;
+	}
 	return Pm;
 }
 
 void Output(Poly P)
 {
 	int flag=0;
-	for(;P;P=P->Next){
+	while(P){
 		if(!flag){
 			printf("%d %d",P->coef,P->expo);
 		}if(flag){
-			printf(" %d %d\n",P->coef,P->expo);
+			printf(" %d %d",P->coef,P->expo);
 		}
+		P=P->Next;
+		flag++;
 	}
 }
